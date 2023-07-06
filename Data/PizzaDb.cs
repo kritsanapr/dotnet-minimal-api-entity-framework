d5897ae80ebd1b60c5d861285a43faa0eee7b708
KRITSANA PRASIT <kritsana.prs@axonstech.com>
2023-07-06 13:11:57 +0700

    first time with .Net core minimal api

---
+++ Data/PizzaDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+namespace PizzaStore.Data
+{
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+    }
+
+}
